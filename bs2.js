0b040c023e72b5e252a17451c660729351ddf0ad
Yaroslav Neznaradko <nezn27@yahoo.com>
2016-05-28 00:33:23 +0300

    Add second task

---
+++ bs2.js
@@ -0,0 +1,78 @@
+//main function
+$(document).ready(function(){
+    //add by enter
+    $('#newTask').keyup(function(e)
+    {
+        if (e.keyCode === 13)
+        {
+          var text = $('#newTask').val();
+              $("#todo-list").append('<li ><input type="checkbox" class="toggle"/ ><span class="text">'
+          + text + ' </span><button class="clear"></button></li>');
+          $("#newTask").val('');
+        }
+    });
+    //button variant
+    $('#submit').click(function(){
+      var text = $('#newTask').val();
+          $("#todo-list").append('<li ><input type="checkbox" class="toggle"/ ><span class="text">'
+      + text + ' </span><button class="clear"></button></li>');
+      $("#newTask").val('');
+    });
+    //remove button
+    $(document).on('click','.clear', function(){
+      $(this).parent().remove();
+    });
+    //toggle all tasks
+    $("#toggle-all").click(function ()
+    {
+        $('input:checkbox').not(this).prop('checked', this.checked);
+        if ( $('li').css('textDecoration') == 'line-through' )
+        {
+            $('li').css('textDecoration', 'none');
+            $('li').parent().css('opacity', '1');
+        }
+        else
+        {
+            $('li').css('textDecoration', 'line-through');
+            $('li').parent().css('opacity', '0.5');
+        }
+    });
+    //single toggle
+    $(document).on('click', '.toggle', function(){
+      if ($(this).parent().css('textDecoration') == 'line-through')
+      {
+           $(this).parent().css('textDecoration', 'none');
+           $(this).parent().css('opacity', '1');
+      }
+      else
+      {
+           $(this).parent().css('textDecoration', 'line-through');
+           $(this).parent().css('opacity', '0.50');
+      }
+    });
+    //clear completed tasks
+    $("#clearcompleted").click(function(){
+      $("#todo-list .toggle:checked").parent().remove();
+    });
+    $('#todo-list').on('dblclick', 'span', function ()
+        {
+            var thisData = this.innerHTML,
+            $el = $('<input type="text" class="in-edit-text"/>');
+            $(this).replaceWith($el);
+            $el.val(thisData).focus();
+            $(this).find(".text").hide();
+            $(this).find(".clear").hide();
+        }
+    );
+    //enter and esc events
+    $('#todo-list').on('keyup', '.in-edit-text', (function(e)
+    {
+        if (e.keyCode === 13)
+        {
+            $(this).replaceWith($('<span class="text">' + $(this).val() + '</span>'));
+        }
+        if (e.keyCode == 27) {
+            $('.in-edit-text').remove();
+        }
+    }));
+});
